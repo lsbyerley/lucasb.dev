6355214d0dd00c21c3d5f9ea39b3bfea571b1f24
lsbyerley <lsbyerley@gmail.com>
2019-12-29 22:33:07 -0500

    add swanson img

--- src/pages/SwansonQuote.vue
+++ src/pages/SwansonQuote.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="wrapper" class="flex flex-col min-h-screen p-3 relative  font-light">
-    <div id="main" class="flex items-center justify-center flex-grow-0 flex-shrink-0  w-full rounded-lg text-center">
-      <div class="inner w-full  relative">
+  <div id="wrapper" class="flex flex-col min-h-screen p-3 relative font-light">
+    <div id="main" class="flex items-center justify-center flex-grow-0 flex-shrink-0 w-full rounded-lg text-center">
+      <div class="inner w-full relative">
 
-        <div class="bg-trans rounded-sm p-10">
+        <div class="bg-trans rounded-sm p-4 md:p-6">
           <h2 class="text-gray-200 font-bold text-3xl border-b-4 border-gray-600 pb-4 mb-4">Random Swanson</h2>
           <blockquote class="border-l-2 border-blue-400 py-2 pl-3 pr-4 mx-auto my-4 text-gray-400 bg-gray-700">
             <p class="mb-4 font-semibold">{{ randomQuote }}</p>
             <span>-</span>
             <cite>Ron Swanson</cite>
           </blockquote>
+          <div class="w-32 h-40 m-auto">
+            <img @click="newQuote" src="~/src/assets/images/swanson.png" />
+          </div>
         </div>
 
       </div>
@@ -26,13 +29,19 @@ export default {
   name: 'SwansonQuote',
   data() {
     return {
+      randomQuote: '',
       quotes: quotes
     }
   },
-  computed: {
-    randomQuote() {
-      let ran = _random(0, quotes.length-1)
-      return this.quotes[ran].text
+  created() {
+    let ran = _random(0, quotes.length-1)
+    this.randomQuote = this.quotes[ran].text
+  },
+  methods: {
+    newQuote() {
+      let ranNum = _random(0, quotes.length-1)
+      let ranQuote = this.quotes[ranNum].text
+      this.randomQuote = ranQuote
     }
   }
 }
