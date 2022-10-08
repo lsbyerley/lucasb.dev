7cc2e9111a56e82b7ed69f0ba586c0807831c438
lsbyerley <lsbyerley@gmail.com>
2022-10-08 10:52:09 -0400

    feat: updated uses page

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -44,79 +44,39 @@ export default function Uses() {
         intro="Here’s a small list of all of my favorite stuff."
       >
         <div className="space-y-20">
-          <p className="text-gray-600 dark:text-gray-200">list coming soon..</p>
-          {/*<ToolsSection title="Workstation">
-            <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-              I was using an Intel-based 16” MacBook Pro prior to this and the
-              difference is night and day. I’ve never heard the fans turn on a
-              single time, even under the incredibly heavy loads I put it
-              through with our various launch simulations.
+          <ToolsSection title="Workstation">
+            <Tool title="16” MacBook Pro, 32GB RAM (2019)">
+              Running a docker build with vscode open and a chromium based
+              browser has this thing spinning so loud. Ready for an upgrade!
             </Tool>
-            <Tool title="Apple Pro Display XDR (Standard Glass)">
-              The only display on the market if you want something HiDPI and
-              bigger than 27”. When you’re working at planetary scale, every
-              pixel you can get counts.
+            <Tool title="Samsung 32” Class UR59 Series 4K UHD Curved Monitor">
+              Picked this up at costco when it was discounted and I really like
+              it.
             </Tool>
-            <Tool title="IBM Model M SSK Industrial Keyboard">
-              They don’t make keyboards the way they used to. I buy these any
-              time I see them go up for sale and keep them in storage in case I
-              need parts or need to retire my main.
+            <Tool title="Apple Magic Mouse and Keyboard">
+              The standard magic mouse/keyboard 2 options from Apple.
             </Tool>
-            <Tool title="Apple Magic Trackpad">
-              Something about all the gestures makes me feel like a wizard with
-              special powers. I really like feeling like a wizard with special
-              powers.
+            <Tool title="Autonomous ErgoChair Core">
+              Pretty solid chair at a decent price. May upgrade to Herman Miller
+              one day
             </Tool>
-            <Tool title="Herman Miller Aeron Chair">
-              If I’m going to slouch in the worst ergonomic position imaginable
-              all day, I might as well do it in an expensive chair.
+            <Tool title="Autonomous SmartDesk Core">
+              Really like the ability to stand when I want to. Nicely priced
+              standing desk option
             </Tool>
           </ToolsSection>
           <ToolsSection title="Development tools">
-            <Tool title="Sublime Text 4">
-              I don’t care if it’s missing all of the fancy IDE features
-              everyone else relies on, Sublime Text is still the best text
-              editor ever made.
-            </Tool>
-            <Tool title="iTerm2">
-              I’m honestly not even sure what features I get with this that
-              aren’t just part of the macOS Terminal but it’s what I use.
-            </Tool>
-            <Tool title="TablePlus">
-              Great software for working with databases. Has saved me from
-              building about a thousand admin interfaces for my various projects
-              over the years.
+            <Tool title="VS Code">
+              Love all the extensions available in VS Code. It's my go to editor
+              these days
             </Tool>
           </ToolsSection>
-          <ToolsSection title="Design">
-            <Tool title="Figma">
-              We started using Figma as just a design tool but now it’s become
-              our virtual whiteboard for the entire company. Never would have
-              expected the collaboration features to be the real hook.
+          <ToolsSection title="Collaboration">
+            <Tool title="Slack">
+              collaboration and communication between co-workers is vital and
+              Slack is our go to.
             </Tool>
           </ToolsSection>
-          <ToolsSection title="Productivity">
-            <Tool title="Alfred">
-              It’s not the newest kid on the block but it’s still the fastest.
-              The Sublime Text of the application launcher world.
-            </Tool>
-            <Tool title="Reflect">
-              Using a daily notes system instead of trying to keep things
-              organized by topics has been super powerful for me. And with
-              Reflect, it’s still easy for me to keep all of that stuff
-              discoverable by topic even though all of my writing happens in the
-              daily note.
-            </Tool>
-            <Tool title="SavvyCal">
-              Great tool for scheduling meetings while protecting my calendar
-              and making sure I still have lots of time for deep work during the
-              week.
-            </Tool>
-            <Tool title="Focus">
-              Simple tool for blocking distracting websites when I need to just
-              do the work and get some momentum going.
-            </Tool>
-  </ToolsSection>*/}
         </div>
       </SimpleLayout>
     </>
