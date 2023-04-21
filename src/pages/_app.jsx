9a3dc3cbc029fe33b4706d40d71e424441311ee6
lsbyerley <lsbyerley@gmail.com>
2023-04-20 22:26:58 -0400

    remove panelbear and rainbowkit

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,8 +4,6 @@ import ContextProvider from '@/providers/ContextProvider';
 import ApolloProvider from '@/providers/ApolloProvider';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { usePanelbear } from '@panelbear/panelbear-nextjs';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { polygon, polygonMumbai } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
@@ -20,7 +18,6 @@ import {
 } from '@lens-protocol/react-web';
 import { bindings as wagmiBindings } from '@lens-protocol/wagmi';
 
-import '@rainbow-me/rainbowkit/styles.css';
 import '@/styles/tailwind.css';
 import 'focus-visible';
 
@@ -42,14 +39,8 @@ const { chains, provider } = configureChains(
   ]
 );
 
-/* const { connectors } = getDefaultWallets({
-  appName: 'Lens Social Page - lucasb.dev',
-  chains,
-}); */
-
 const wagmiClient = createClient({
   autoConnect: true,
-  // connectors,
   provider,
 });
 
@@ -66,35 +57,24 @@ function usePrevious(value) {
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname);
 
-  // PanelBear hook
-  usePanelbear(process.env.NEXT_PUBLIC_PANEL_BEAR_ID || '', {
-    // remove comment to send events on localhost
-    // debug: true,
-  });
-
   return (
     <ContextProvider>
       <WagmiConfig client={wagmiClient}>
         <LensProvider config={lensConfig}>
-          <RainbowKitProvider chains={chains}>
-            <ApolloProvider>
-              <div className="fixed inset-0 flex justify-center sm:px-8">
-                <div className="flex w-full max-w-7xl lg:px-8">
-                  <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-                </div>
+          <ApolloProvider>
+            <div className="fixed inset-0 flex justify-center sm:px-8">
+              <div className="flex w-full max-w-7xl lg:px-8">
+                <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
               </div>
-              <div className="relative">
-                <Header />
-                <main>
-                  <Component
-                    previousPathname={previousPathname}
-                    {...pageProps}
-                  />
-                </main>
-                <Footer />
-              </div>
-            </ApolloProvider>
-          </RainbowKitProvider>
+            </div>
+            <div className="relative">
+              <Header />
+              <main>
+                <Component previousPathname={previousPathname} {...pageProps} />
+              </main>
+              <Footer />
+            </div>
+          </ApolloProvider>
         </LensProvider>
       </WagmiConfig>
     </ContextProvider>
